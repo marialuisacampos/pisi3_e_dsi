7cc03c5c8a6754164584a2d4a2329d03617bc321
marialuisacampos <luisa.campos@ufrpe.br>
2023-01-02 20:26:57 -0300

    input, logo e botoes de cadastro

--- dsi/easyfood/lib/pages/signupPage.dart
+++ dsi/easyfood/lib/pages/signupPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:easyfood/pages/signinPage.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -9,15 +11,121 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.arrow_back),
-          color: Colors.black,
-          onPressed: () => Navigator.of(context).pop(),
+        backgroundColor: Color(0xfff2f2f2),
+        appBar: AppBar(
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back),
+            color: Colors.black,
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          backgroundColor: Colors.white,
         ),
-        backgroundColor: Colors.white,
-      ),
-      // body: ...
-    );
+        body: Padding(
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              "assets/images/logotipo.jpg",
+                            ),
+                            fit: BoxFit.fill)),
+                  ),
+                  Text(
+                    "EasyFood",
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        labelText: "Nome Completo",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 15)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        labelText: "Data de Nascimento",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 15)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 15)),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 15)),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Repetir Senha",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 15)),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 50.0,
+                    width: 230.0,
+                    child: ElevatedButton(
+                      child: Text('Cadastrar'),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          backgroundColor: Color(0xff29af6f),
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignInPage()));
+                      },
+                    ),
+                  ),
+                ]),
+          ),
+          padding: EdgeInsets.all(50),
+        ));
   }
 }
