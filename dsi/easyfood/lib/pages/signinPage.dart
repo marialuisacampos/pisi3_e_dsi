47d461fa036c00da96f2d4c4a5199667c790f362
marialuisacampos <luisa.campos@ufrpe.br>
2023-01-02 18:56:34 -0300

    inputs de login

--- dsi/easyfood/lib/pages/signinPage.dart
+++ dsi/easyfood/lib/pages/signinPage.dart
@@ -9,15 +9,43 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.arrow_back),
-          color: Colors.black,
-          onPressed: () => Navigator.of(context).pop(),
+        appBar: AppBar(
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back),
+            color: Colors.black,
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          backgroundColor: Colors.white,
         ),
-        backgroundColor: Colors.white,
-      ),
-      // body: ...
-    );
+        body: Padding(
+          child: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 20)),
+                  ),
+                  Divider(),
+                  TextFormField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                            color: Color.fromRGBO(30, 30, 30, 100),
+                            fontSize: 20)),
+                  )
+                ]),
+          ),
+          padding: EdgeInsets.all(50),
+        ));
   }
 }
