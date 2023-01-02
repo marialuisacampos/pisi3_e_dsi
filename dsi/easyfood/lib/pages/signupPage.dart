552dae5d6f98c9c33e18b14e7e81a35c8ce70069
marialuisacampos <luisa.campos@ufrpe.br>
2023-01-02 20:43:15 -0300

    fix bug com scroll

--- dsi/easyfood/lib/pages/signupPage.dart
+++ dsi/easyfood/lib/pages/signupPage.dart
@@ -22,108 +22,117 @@ class _SignUpPageState extends State<SignUpPage> {
         ),
         body: Padding(
           child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage(
-                              "assets/images/logotipo.jpg",
-                            ),
-                            fit: BoxFit.fill)),
-                  ),
-                  Text(
-                    "EasyFood",
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                "assets/images/logotipo.jpg",
+                              ),
+                              fit: BoxFit.fill)),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                        labelText: "Nome Completo",
-                        labelStyle: TextStyle(
-                            color: Color.fromRGBO(30, 30, 30, 100),
-                            fontSize: 15)),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                        labelText: "Data de Nascimento",
-                        labelStyle: TextStyle(
-                            color: Color.fromRGBO(30, 30, 30, 100),
-                            fontSize: 15)),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                            color: Color.fromRGBO(30, 30, 30, 100),
-                            fontSize: 15)),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: "Senha",
-                        labelStyle: TextStyle(
-                            color: Color.fromRGBO(30, 30, 30, 100),
-                            fontSize: 15)),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: "Repetir Senha",
-                        labelStyle: TextStyle(
-                            color: Color.fromRGBO(30, 30, 30, 100),
-                            fontSize: 15)),
-                  ),
-                  SizedBox(height: 30),
-                  Container(
-                    height: 50.0,
-                    width: 230.0,
-                    child: ElevatedButton(
-                      child: Text('Cadastrar'),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                          backgroundColor: Color(0xff29af6f),
-                          textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.bold)),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignInPage()));
-                      },
+                    Text(
+                      "EasyFood",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ]),
+                    SizedBox(height: 12),
+                    Text('Cadastro',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Divider(),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          labelText: "Nome Completo",
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(30, 30, 30, 100),
+                              fontSize: 15)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          labelText: "Data de Nascimento",
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(30, 30, 30, 100),
+                              fontSize: 15)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(30, 30, 30, 100),
+                              fontSize: 15)),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "Senha",
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(30, 30, 30, 100),
+                              fontSize: 15)),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "Confirmar Senha",
+                          labelStyle: TextStyle(
+                              color: Color.fromRGBO(30, 30, 30, 100),
+                              fontSize: 15)),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      height: 50.0,
+                      width: 230.0,
+                      child: ElevatedButton(
+                        child: Text('Cadastrar'),
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            backgroundColor: Color(0xff29af6f),
+                            textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignInPage()));
+                        },
+                      ),
+                    ),
+                  ]),
+            ),
           ),
           padding: EdgeInsets.all(50),
         ));
